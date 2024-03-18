5623aa562fe10010b51cb2fabc5562595c3f9132
OliviaS <1211024003@zust.edu.cn>
2024-03-18 22:18:03 +0800

    Exception specified

--- ErrorHandling/src/dividedbyzero/Main.java
+++ ErrorHandling/src/dividedbyzero/Main.java
@@ -6,7 +6,7 @@ public class Main {
 	public static void main(String[] args) { 
 		try {
 			System.out.println(3/0); 
-		} catch (Exception e) {
+		} catch (ArithmeticException e) {
 			// print the error message
 			System.out.printf("Caught runtime exception = %s\n", e.toString());
 		}
